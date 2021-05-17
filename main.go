5e5724f869d93aca40baa3c7d7127a3b8123244e
Pranoto Budi <pranotobudi.s@gmail.com>
2021-05-17 11:48:55 +0700

    csrf feature

--- main.go
+++ main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type M map[string]interface{}
 
 func main() {
-	fmt.Println("bismillah")
+	tmpl := template.Must(template.ParseGlob("./*.html"))
+
+	e := echo.New()
+
+	const CSRFTokenHeader = "X-CSRF-Token"
+	const CSRFKey = "csrf"
+
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup: "header:" + CSRFTokenHeader,
+		ContextKey:  CSRFKey,
+	}))
+
+	e.GET("/index", func(c echo.Context) error {
+		data := make(M)
+		data[CSRFKey] = c.Get(CSRFKey)
+		return tmpl.Execute(c.Response(), data)
+	})
+	e.POST("/sayhello", func(c echo.Context) error {
+		data := make(M)
+		data[CSRFKey] = c.Get(CSRFKey)
+		if err := c.Bind(&data); err != nil {
+			return err
+		}
+
+		message := fmt.Sprintf("hello %s", data["name"])
+		return c.JSON(http.StatusOK, message)
+	})
+
+	e.Logger.Fatal(e.Start(":9000"))
 }
